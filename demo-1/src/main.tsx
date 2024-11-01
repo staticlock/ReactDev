d97dd7bb1f39e3d0ae1a7ce84fdd34a8a98e000b
staticlock <2133525474@qq.com>
2024-11-01 22:54:33 +0800

    first commit

--- demo-1/src/main.tsx
+++ demo-1/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 // !为非空断言
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
