0fc0b7a0d920e6f549c9c145d448710c84c56fa6
staticlock <2133525474@qq.com>
2024-11-03 01:16:06 +0800

    2024-11-3

--- demo-1/src/demo/react.tsx
+++ demo-1/src/demo/react.tsx
@@ -1,15 +1,15 @@
-const App = () => {
-    return (<div id="2">
-        <span>小满zs</span>
-    </div>)
-}
+const App = (): JSX.Element => (
+    <div>
+        <span>Hello World</span>
+    </div>
+)
 // 上面这段代码会通过babel或者swc转换成
 // const App = () => {
 //     return React.createElement('div', { id: 2 },
 //         React.createElement('span', null, '小满zs')
 //     );
 // };
-//实现简易虚拟DOM   
+//实现简易虚拟DOM
 const React = {
     //children为子元素
     createElement(type: string, props: {} | null, ...children: any[]) {
@@ -18,13 +18,13 @@ const React = {
             props: {
                 ...props,
                 children: children.map((child) => {
-                    if (typeof (child) === 'object') {
+                    if (typeof child === "object") {
                         return child
                     } else {
                         return React.createTextElement(child)
                     }
-                })
-            }
+                }),
+            },
         }
     },
     // 处理children为文本
@@ -33,10 +33,10 @@ const React = {
             type: "TEXT_TYPE",
             props: {
                 nodeValue: text,
-                children: []
-            }
+                children: [],
+            },
         }
-    }
+    },
 }
 const vdom = React.createElement("div", { id: "1" }, React.createElement("span", null, "hahhhah"))
 export default vdom
